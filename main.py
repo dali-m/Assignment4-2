740cc81e7c5ef27a93b29703b7ddc40484c6ca3e
Dalilah Ali <dali11@hawkmail.hccfl.edu>
2024-09-28 17:32:08 -0400

    Added IF statements and output statements

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 # This program calculates an employee's productivity bonus.
+
+employeeName=str(input("Enter Employee's name: "))
+numShifts=int(input("Enter Number of Shifts: "))
+numTransactions=int(input("Enter Number of Transactions: "))
+tranValue=float(input("Enter transaction dollar value: "))
+
+bonus=0.00
+
+score=(tranValue/numTransactions)/numShifts
+if score <= 30:
+    bonus = 50.00
+elif score >= 31 and score <= 69:
+    bonus = 75.00
+elif score >= 70 and score <= 199:
+    bonus = 100.00
+elif score >= 200:
+    bonus = 200.00
+
+print("Employee Name: ",employeeName)
+print("Employee Bonus: ",bonus)
